6b7443f48324a493c22abab82b699df069dd5ba7
u <58615943+darkisgay@users.noreply.github.com>
2020-05-24 10:46:46 +0700

    Update server_data.php

--- growtopia/server_data.php
+++ growtopia/server_data.php
@@ -1,3 +1,9 @@
-maint|test
-server|38.107.214.220
-port|17045
+server|127.0.0.1
+port|17091
+type|1
+#maint|Server is under maintenance. We will be back online shortly. Thank you for your patience!
+beta_server|127.0.0.1
+beta_port|17091
+beta_type|1
+meta|mymom.com
+RTENDMARKERBS1001
